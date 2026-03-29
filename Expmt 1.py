f849e4655806e012b900a3fc4eb36499dfe6684d
juugi202412956-ship-it <juugi202412956@jainuniversity.ac.in>
2026-03-29 10:45:37 +0530

    Refactor code and update variable names

--- Expmt 1.py
+++ Expmt 1.py
@@ -1,42 +1,35 @@
- #Boolean
-
-#black = True
-#white = False
-#green = False
-
-#print(f"africaines are {black}")
-#print(f"arabes are {white}")
-#print(f"australiens are {green}")
+#Boolean
+is_africaines_black = True
+is_arabes_white = False
+is_australiens_green = False
+print(f"Africaines are black: {is_africaines_black}")
+print(f"Arabes are white: {is_arabes_white}")
+print(f"Australiens are green: {is_australiens_green}")
 
 #string
-#name="sourou_kire"
-#age="theenager"
-#email="souroukire@gmail.com"
-
-#print(f"hi {name}")
-#print(f"what is your {age}")
-#print(f"your email ID {email}")
+name="sourou_kire"
+age="towenty one"
+email="souroukire@gmail.com"
+print(f"hi {name}")
+print(f"your age is:{age}")
+print(f"your email ID:{email}")
 
 
 #float
-#otp=33.02
-#distance=5.3
-#payment=210.75
-
-#print(f"give youre {otp}")
-#print(f"your distance is {distance}km")
-#print(f"your payment is ${payment}")
+otp=33.02
+distance=5.3
+payment=210.75
+print(f"your otp is:{otp}")
+print(f"your distance is:{distance}km")
+print(f"your payment is:${payment}")
 
 #integer
-
-#age=22
-#white=75
-#hight=2
-
-#print(f"i am {age}years old")
-#print(f"your {white}kg")
-#print(f"my hight is:{hight}m")
- 
+age=22
+white=75
+hight=2
+print(f"i am {age}years old")
+print(f"my white is:{white}kg")
+print(f"my hight is:{hight}m")
 #FUNCTION
 def add(x, y):
     z = x + y
@@ -64,5 +57,26 @@ add(x, y)
 subtract(x, y)
 multiply(x, y)
 divide(x, y)
-  
- 
+
+OUTPUT:
+
+Africaines are black: True
+Arabes are white: False
+Australiens are green: False
+
+hi sourou_kire
+your age is:towenty one
+your email ID:souroukire@gmail.com
+
+your otp is:33.02
+your distance is:5.3km
+your payment is:$210.75
+
+i am 22years old
+my white is:75kg
+my hight is:2m
+
+Addition: 7
+Subtraction: -1
+Multiplication: 12
+Division: 0.75
