75981fb95ca4677300ff3888c699dd8b52060bda
juugi202412956-ship-it <juugi202412956@jainuniversity.ac.in>
2026-03-29 10:20:12 +0530

    Add files via upload

---
+++ Expmt 1.py
@@ -0,0 +1,68 @@
+ #Boolean
+
+#black = True
+#white = False
+#green = False
+
+#print(f"africaines are {black}")
+#print(f"arabes are {white}")
+#print(f"australiens are {green}")
+
+#string
+#name="sourou_kire"
+#age="theenager"
+#email="souroukire@gmail.com"
+
+#print(f"hi {name}")
+#print(f"what is your {age}")
+#print(f"your email ID {email}")
+
+
+#float
+#otp=33.02
+#distance=5.3
+#payment=210.75
+
+#print(f"give youre {otp}")
+#print(f"your distance is {distance}km")
+#print(f"your payment is ${payment}")
+
+#integer
+
+#age=22
+#white=75
+#hight=2
+
+#print(f"i am {age}years old")
+#print(f"your {white}kg")
+#print(f"my hight is:{hight}m")
+ 
+#FUNCTION
+def add(x, y):
+    z = x + y
+    print("Addition:", z)
+
+def subtract(x, y):
+    z = x - y
+    print("Subtraction:", z)
+
+def multiply(x, y):
+    z = x * y
+    print("Multiplication:", z)
+
+def divide(x, y):
+    if y != 0:
+        z = x / y
+        print("Division:", z)
+    else:
+        print("Cannot divide by zero")
+
+x = 3
+y = 4
+
+add(x, y)
+subtract(x, y)
+multiply(x, y)
+divide(x, y)
+  
+ 
